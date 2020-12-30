26c4b68c7de60e659803e2364fad9cae3e7d6118
Didrik Sten Ingebrigtsen <didrik@Oestrem--Steen-FirmaPC.local>
2020-12-31 00:14:38 +0100

    Documented, made names more consistent, cleaned up

--- scrapeEachCourse.py
+++ scrapeEachCourse.py
@@ -1,16 +1,32 @@
+"""Functions for scraping courses."""
+
 import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
 from scrapeForCourses import make_soup
 
-def is_clean(strg, search=re.compile(r'[<>\\\n]').search):
-        return not bool(search(strg))
+def is_clean(string):
+    """Checks if string has no very special characters.
+
+    :param string: String to check.
+
+    :retur: Boolean indicating whether there are special characters or not.
+    """
+    search = re.compile(r'[<>\\\n]').search
+    return not search(string)
     
 def make_courselist(coursecodes, course_relations):
-    # Merge coursecodes and course_relations into one nested list
-    # Final list looks like [['either this', 'or this'], ['also either this', 'or this'], 'and this']
-    print(coursecodes, course_relations)
+    """Merge coursecodes and course_relations into one nested list.
+
+    :param coursecodes: List of course code strings.
+    :param course_relations: List of strings where element i specifies the 
+                             relation between element i-1 and i in the coursecodes list.
+                             Each element can be either 'interchangeable' or 'none'.
+
+    :return: List of coursecodes in a nested list. List looks like
+             [['either this course', 'or this'], ['also either this', 'or this'], 'and this']
+    """
     courselist = []
     current_group = []
     for code, relation in zip(coursecodes, course_relations):
@@ -19,25 +35,37 @@ def make_courselist(coursecodes, course_relations):
         elif current_group != []:
             current_group.append(code)
             courselist.append(current_group)
-            # print(f"Current group: {current_group}")
             current_group = []
         else:
             courselist.append(code)
 
     return courselist
 
-def get_courses(text):
-    # Find all courses mentioned in the text
+def get_courses(string):
+    """Find all mentions of course codes in a string, and their relations.
+
+    Courses can either be interchangeable (eg 'To take this course, you have to
+    take either course A or course B'), or not.
+    
+    :param string: String with possible course codes used.
+
+    :return: 2-tuple of course codes list and course relations list.
+             In the course relations list element i specifies the 
+             relation between element i-1 and i in the course codes list.
+             Each element can be either 'interchangeable' or 'none'.
+    """
+
+    # Find all courses mentioned in the string
     coursecodes, coursecodes_indexes = [], []
-    for match in re.finditer(r"[A-ZÆØÅ\-]+\d{4}", text):
+    for match in re.finditer(r"[A-ZÆØÅ\-]+\d+[A-ZÆØÅ\-]{0,6}\d{0,2}", string):
         coursecodes.append(match.group(0))
         coursecodes_indexes.append([match.start(), match.end()])
 
     # Find special cases where interchangeability is implied by something other than '/'
     special_group_indexes = []
-    for beginning in re.finditer(r"Ett av emnene|One of the courses", text):
+    for beginning in re.finditer(r"Ett av emnene|One of the courses", string):
         special_group_indexes.append([beginning.end()])
-        end = re.search(r".", text[special_group_indexes[-1][0]:])
+        end = re.search(r".", string[special_group_indexes[-1][0]:])
         if end:
             special_group_indexes[-1].append(end.start())
 
@@ -59,24 +87,35 @@ def get_courses(text):
                 in_special_group = 0
         
         # Check if there's a '/' or an 'or' between the codes, implying interchangeability
-        if re.search(r"\/|[ (]eller |[ (]or ", text[start:end]) and not re.search(r"og ", text[start:end]):
+        if re.search(r"\/|[ (]eller |[ (]or ", string[start:end]) and not re.search(r"og ", string[start:end]):
             course_relations.append('interchangeable')
         else:
             course_relations.append('none')
     course_relations.append('none')
 
-    return make_courselist(coursecodes, course_relations)
+    return coursecodes, course_relations
 
 def get_prerequisites(content_tag):
+    """Makes lists of prerequisites for a course, given the content tag of the course.
+
+    :param content_tag: bs4.BeautifulSoup instance of content tag in course page.
+
+    :return: 2-tuple of obligatory and recommended lists, with format as described in make_courselist().
+    """
     descendants = []
+    if content_tag is None:
+        # Occasional error that isn't impossible to handle,
+        # but that occurs so rarely and with courses UiO has
+        # themselves made an error, so this should do
+        return [], []
+    
     for child in content_tag.descendants:
         if child.name == 'a':
             continue
-            #descendants.append(child.string)
         else:
             descendants.append(str(child))
 
-    pure_text = ' '.join([ strg for strg in descendants if is_clean(strg) ])
+    pure_text = ' '.join([string for string in descendants if is_clean(string)])
     #print(f"Pure text: {pure_text}")
 
     # Find where the sections start and end
@@ -86,13 +125,11 @@ def get_prerequisites(content_tag):
     oblig_match = re.search(r'Formal prerequisites|Obligatoriske forkunnskaper', pure_text)
     if oblig_match:
         obligatory_start = oblig_match.end()
-        print(f"Fant oblig, og den starter på {obligatory_start}")
 
     recommended_match = re.search(r'Recommended previous knowledge|Anbefalte forkunnskaper', pure_text)
     if recommended_match:
         obligatory_end = recommended_match.start()
         recommended_start = recommended_match.end()
-        print(f"Fant anbefalt, og den starter på {recommended_start}")
 
     end_search_beginning = obligatory_start if obligatory_start > recommended_start else recommended_start
     end_of_prerequisites_match = re.search(r'Overlapping courses|Overlappende emner|Undervisning|Teaching', pure_text[end_search_beginning:])
@@ -100,43 +137,39 @@ def get_prerequisites(content_tag):
         recommended_end = end_of_prerequisites_match.start() + end_search_beginning
         if recommended_start == -2:
             obligatory_end = recommended_end
-        print(f"Fant slutten, den er {recommended_end}. Startet søket ved {end_search_beginning}")
 
     # Use get_courses to make a list of all courses in each section, if the section exists
     if ((obligatory_start, obligatory_end) != (-2, -1)):
-        # print(f"Søker gjennom obligatorisk tekst: {pure_text[obligatory_start:obligatory_end]}")
-        obligatory_list = get_courses(pure_text[obligatory_start:obligatory_end])
-        # print(f"Resultat: {obligatory_list}")
+        coursecodes, course_relations = get_courses(pure_text[obligatory_start:obligatory_end])
+        obligatory_list = make_courselist(coursecodes, course_relations)
     else:
         obligatory_list = '0'
 
     if ((recommended_start, recommended_end) != (-2, -1)):
-        # print(f"Søker gjennom anbefalt tekst: {pure_text[recommended_start:recommended_end]}")
-        recommended_list = get_courses(pure_text[recommended_start:recommended_end])
-        # print(f"Resultat: {recommended_list}")
+        coursecodes, course_relations = get_courses(pure_text[recommended_start:recommended_end])
+        recommended_list = make_courselist(coursecodes, course_relations)
     else:
         recommended_list = '0'
 
     return obligatory_list, recommended_list
 
-def scrape_course(faculty, institute, coursecode):
-    print(f'Scraping {coursecode}')
-
-    course_url = 'https://www.uio.no/studier/emner/' + faculty + '/' + institute + '/' + coursecode + '/'
-    course_soup = make_soup(course_url)
-
-    content = course_soup.find(id='vrtx-course-content')
-    obligatory, recommended = get_prerequisites(content)
-
-    return obligatory, recommended
-
 if __name__ == '__main__':
     courseData = pd.read_pickle('courses.pkl')
+    num_courses = len(courseData.index)
 
     obligatories, recommendeds = [], []
 
-    for code, course in courseData.iterrows():
-        obligatory, recommended = scrape_course(course['faculty'], course['institute'], course['coursecode'])
+    for i, (code, course) in enumerate(courseData.iterrows()):
+        print(f"\r|{'='*(i*50//num_courses)}{' '*(50-i*50//num_courses)}| {i/num_courses:.2%} Scraping {course['coursecode']}\033[K", flush=True, end='')
+
+        course_url = 'https://www.uio.no/studier/emner/'\
+                   + course['faculty'] + '/'\
+                   + course['institute'] + '/'\
+                   + course['coursecode'] + '/'
+        course_soup = make_soup(course_url)
+
+        content = course_soup.find(id='vrtx-course-content')
+        obligatory, recommended = get_prerequisites(content)
         obligatories.append(obligatory)
         recommendeds.append(recommended)
 
@@ -144,3 +177,5 @@ if __name__ == '__main__':
     courseData['recommended'] = recommendeds
 
     courseData.to_pickle('courses.pkl')
+
+    print("\rScraped all courses, and updated dataframe in 'courses.pkl'\033[K")
--- search.py
+++ search.py
@@ -1,24 +1,47 @@
+"""Interface for searching through course data."""
+
 import pandas as pd
 import re
 import itertools
 
-def code_in(code, codelist):
-    for level1 in codelist:
-        if type(level1) is list:
+def code_in(course, courselist):
+    """Check if course is in nested list, and return list of other courses in that list.
+
+    :param course: Course code, string.
+    :param courselist: List with courses and lists of courses as elements.
+                       The sublists represent interchangeable courses.
+
+    :return: List with all other elements than the one containing the course.
+             False if course is not in list.
+    """
+    for level1 in courselist:
+        if isinstance(level1, list):
             for level2 in level1:
-                if code == level2:
-                    codereturn = codelist.copy()
+                if course == level2:
+                    codereturn = courselist.copy()
                     codereturn.remove(level1)
                     return codereturn
-        elif code == level1:
-            codereturn = codelist.copy()
-            codereturn.remove(code)
+    
+        elif course == level1:
+            codereturn = courselist.copy()
+            codereturn.remove(course)
             return codereturn
     return False
 
-def text_from_codelist(pretext, codelist, posttext, errortext=''):
+def text_from_courselist(prefix, courselist, suffix, errortext=''):
+    """Makes a string out of a list of courses.
+
+    :param prefix: Prefix string.
+    :param courselist: List with courses and lists of courses as elements.
+                       The sublists represent interchangeable courses.
+    :param suffix: Suffix string.
+    :param errortext: String to return if courselist is empty.
+                      Defaults to ''
+
+    :return: String that explains the rrelationship between the courses in the list.
+    """
     text = ''
-    for level1 in codelist:
+    for level1 in courselist:
         if type(level1) is list:
             text += 'enten '
             for level2 in level1:
@@ -27,53 +50,105 @@ def text_from_codelist(pretext, codelist, posttext, errortext=''):
         else:
             text += level1 + ' og '
     text = text[:-4]
-    return pretext + text + posttext if text != '' else errortext
+    return prefix + text + suffix if text != '' else errortext
 
-def new_root(coursecode, obligatory_list, recommended_list, checked_courses):
-    new_obligatory, new_recommended, new_checked_courses = grow_roots(coursecode, checked_courses)
-    obligatory_list.extend(new_obligatory if isinstance(new_obligatory, list) else [new_obligatory])
-    recommended_list.extend(new_recommended if isinstance(new_recommended, list) else [new_recommended])
-    checked_courses.extend(new_checked_courses if isinstance(new_checked_courses, list) else [new_checked_courses])
+def listify(element):
+    """Turns something into a list if it isn't already a list"""
+    return element if isinstance(element, list) else [element]
+
+def new_root(course, obligatory_list, recommended_list, checked_courses, course_df):
+    """
+
+    :param course: Course code, string.
+    :param obligatory_list:
+    :param recommended_list:
+    :param checked_courses:
+    
+    :return: 3-tuple with lists of obligatory, recommended and checked courses
+    """
+    print(type(obligatory_list))
+    new_obligatory, new_recommended, new_checked_courses = grow_roots(course, checked_courses, course_df)
+    obligatory_list.extend(listify(new_obligatory))
+    recommended_list.extend(listify(new_recommended))
+    checked_courses.extend(listify(new_checked_courses))
 
     return obligatory_list, recommended_list, checked_courses
 
-def grow_roots(input_code, checked_courses):
-    if input_code in courseData.index and input_code not in checked_courses:
-        checked_courses.append(input_code)
-        obligatory_list = courseData.at[input_code, 'obligatory']
-        recommended_list = courseData.at[input_code, 'recommended']
+def grow_roots(course, checked_courses, course_df):
+    """Makes lists of courses that are obligatory and recommended precursors to a course.
+
+    Works recursively with new_root() to walk down the tree. To prevent it from infinite 
+    loops it also returns courses checked.
+
+    :param course: Course code, string.
+    :param checked_courses: List of course codes to ignore.
+    :param course_df: pandas.DataFrame instance with data.
+
+    :return: 3-tuple of lists of obligatory, recommended, and checked courses.
+    """
+    if course in course_df.index and course not in checked_courses:
+        checked_courses.append(course)
+        obligatory_list = course_df.at[course, 'obligatory']
+        recommended_list = course_df.at[course, 'recommended']
         if obligatory_list != []:
             for obligatory_element in obligatory_list:
                 if isinstance(obligatory_element, list):
                     for obligatory_course in obligatory_element:
-                        obligatory_list, recommended_list, checked_courses = new_root(obligatory_course, obligatory_list, recommended_list, checked_courses)
+                        obligatory_list, recommended_list, checked_courses = \
+                            new_root(
+                                obligatory_course,
+                                obligatory_list,
+                                recommended_list,
+                                checked_courses,
+                                course_df
+                            )
                 else:
-                    obligatory_course = obligatory_element
-                    obligatory_list, recommended_list, checked_courses = new_root(obligatory_course, obligatory_list, recommended_list, checked_courses)
+                    obligatory_list, recommended_list, checked_courses = \
+                        new_root(
+                            obligatory_element,
+                            obligatory_list,
+                            recommended_list,
+                            checked_courses,
+                            course_df
+                        )
 
         return obligatory_list, recommended_list, checked_courses
     return [], [], []
 
 def duplicate_or_super(this_list, other_list):
+    """Checks if two lists are identical or one is a superset of the other.
+
+    :param this_list: Nested list of courses.
+    :param other_list: Nested list of courses.
+
+    :return: Bool. True if duplicate or first is super, False if not.
+    """
     this_copy = this_list.copy()
     other_copy = other_list.copy()
     for this_item in this_list:
         for other_item in other_list:
             if this_item == other_item:
-                #print(f"Denne tingen: {this_item} og denne listen {this_list}")
                 this_copy.remove(this_item)
                 other_copy.remove(other_item)
     if this_copy == []:
-        print(this_list, other_list)
         return True
     else:
         return False
 
 def untangle_roots(messy_roots):
+    """Cleans up list of courses, removing duplicate information.
+
+    Other than removing complete duplicates, it also removes redundant information.
+    Say that to take course A, you have to take course B and D. To take B, you must take C or D.
+    This function would return that you only have to take B and D to take course A.
+
+    :param messy_roots: Nested list of courses.
+
+    :return: Nested list of courses.
+    """
     messy_roots_copy = list(messy_roots)[0].copy()
     untangled_roots = []
     if messy_roots_copy != []:
-        #print(messy_roots_copy)
         for messy_root in messy_roots[0]:
             if isinstance(messy_root, str):
                 untangled_roots.append(messy_root)
@@ -93,31 +168,49 @@ def untangle_roots(messy_roots):
     untangled_roots.extend(messy_roots_copy)
     return untangled_roots
 
-def search_single_course(input_code, courseData, flags):
+def search_single_course(course, course_df, flags):
+    """Prints out text describing what courses must be taken before taking a given course.
 
+    :param course: Course code, string.
+    :param course_df: pandas.DataFrame instance with data.
+    :param flags: Flags that change what's printed out. Supported flags are:
+                        'compact' or 'c': Removes whitespace and other courses required
+                                          to take a course the input is a precursor to.
+                        'roots' or 'r': Also prints out info about courses that themselves
+                                        are precursors to the input course.
+    
+    :return: Bool. Whether or not any results could be found.
+    """
     text_index = 0
     for flag in flags:
         if flag == 'compact' or flag == 'c':
             text_index = 1
         elif flag == 'roots' or flag == 'r':
-            messy_roots = grow_roots(input_code, [])[0:2]
+            messy_roots = grow_roots(course, [], course_df)[0:2]
             untangled_roots = untangle_roots(messy_roots)
-            print(text_from_codelist(f"For å ta {input_code} må du først ta ", untangled_roots, '.', errortext='Fant ingen obligatoriske forkunnskapskrav.'))
+            print(
+                text_from_courselist(
+                    f"For å ta {course} må du først ta ",
+                    untangled_roots,
+                    '.',
+                    errortext='Fant ingen obligatoriske forkunnskapskrav.'
+                )
+            )
 
     results = False
-    print(f'\n---\nSøker etter emner {input_code} peker mot...')
-    for index, course in courseData.iterrows():
+    print(f'\n---\nSøker etter emner {course} peker mot...')
+    for index, other_course_row in course_df.iterrows():
         total_text = ['\nEmnet ', '']
-        obligatory_list = code_in(input_code, course['obligatory'])
-        recommended_list = code_in(input_code, course['recommended'])
+        obligatory_list = code_in(course, other_course_row['obligatory'])
+        recommended_list = code_in(course, other_course_row['recommended'])
         if obligatory_list:
-            total_text[0] += f"leder til {course['coursecode']} - {course['coursename']}{text_from_codelist(', hvis du også tar ', obligatory_list, '')}."
-            total_text[1] += f"{course['coursecode']} - {course['coursename']} (obligatorisk)"
+            total_text[0] += f"leder til {other_course_row['coursecode']} - {other_course_row['coursename']}{text_from_courselist(', hvis du også tar ', obligatory_list, '')}."
+            total_text[1] += f"{other_course_row['coursecode']} - {other_course_row['coursename']} (obligatorisk)"
             if recommended_list:
-                total_text[0] += text_from_codelist('', recommended_list, ' er anbefalte forkunnskaper.')
+                total_text[0] += text_from_courselist('', recommended_list, ' er anbefalte forkunnskaper.')
         elif recommended_list:
-            total_text[0] += f"er en anbefalt forkunnskap til {course['coursecode']} - {course['coursename']}{text_from_codelist(', sammen med ', recommended_list, '')}.{text_from_codelist(' Den obligatoriske forkunnskapen får du gjennom ', course['obligatory'], '.')}"
-            total_text[1] += f"{course['coursecode']} - {course['coursename']} (anbefalt)"
+            total_text[0] += f"er en anbefalt forkunnskap til {other_course_row['coursecode']} - {other_course_row['coursename']}{text_from_courselist(', sammen med ', recommended_list, '')}.{text_from_courselist(' Den obligatoriske forkunnskapen får du gjennom ', other_course_row['obligatory'], '.')}"
+            total_text[1] += f"{other_course_row['coursecode']} - {other_course_row['coursename']} (anbefalt)"
 
         chosen_text = total_text[text_index]
         
@@ -125,17 +218,17 @@ def search_single_course(input_code, courseData, flags):
             results = True
             print(chosen_text)
     
-
     if results:
         print('\n---\n')
     else:
-        print(f'Fant dessverre ingen emner {input_code} leder til.')
+        print(f'Fant dessverre ingen emner {course} leder til.')
 
     return results
 
 if __name__ == '__main__':
-    courseData = pd.read_pickle('courses.pkl')
-    courseData.set_index('coursecode', drop = False, inplace = True)
+    course_df = pd.read_pickle('courses.pkl')
+    print(course_df)
+    course_df.set_index('coursecode', drop=False, inplace=True)
 
     print('Skriv inn en emnekode du vil se hva slags muligheter gir senere. Skriv \"-help\" for å se kommandoer og få hjelp.')
 
@@ -152,7 +245,7 @@ if __name__ == '__main__':
     run = True
     while(run):
         command = input('Emnekode: ')
-        input_code = re.search(r"^ *([A-ZÆØÅ\-]+\d*[A-ZÆØÅ\-]{0,6}\d{0,2})", command.upper()).group(1)
+        course = re.search(r"^ *([A-ZÆØÅ\-]+\d*[A-ZÆØÅ\-]{0,6}\d{0,2})", command.upper()).group(1)
 
         flags = []
         for flag in re.finditer(r"-(\w+)", command):
@@ -162,5 +255,5 @@ if __name__ == '__main__':
             if flag_str == 'h' or flag_str == 'help':
                 print(help_text)
 
-        if input_code[0] != '-':
-            results = search_single_course(input_code, courseData, flags)
+        if course[0] != '-':
+            results = search_single_course(course, course_df, flags)
