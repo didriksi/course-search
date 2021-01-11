9eca879b24ecbba6ca669c0c620a0617e386e130
Didrik Sten Ingebrigtsen <didrik@eduroam-193-157-227-85.wlan.uio.no>
2021-01-11 12:33:08 +0100

    Clean up search, remove unused functions

--- search.py
+++ search.py
@@ -6,76 +6,6 @@ import itertools
 
 from CourseList import CourseListPrimitive, CompoundCourseList
 
-def code_in(course, courselist):
-    """Check if course is in nested list, and return list of other courses in that list.
-
-    :param course: Course code, string.
-    :param courselist: List with courses and lists of courses as elements.
-                       The sublists represent interchangeable courses.
-
-    :return: List with all other elements than the one containing the course.
-             False if course is not in list.
-    """
-    for level1 in courselist:
-        if isinstance(level1, list):
-            for level2 in level1:
-                if course == level2:
-                    codereturn = courselist.copy()
-                    codereturn.remove(level1)
-                    return codereturn
-    
-        elif course == level1:
-            codereturn = courselist.copy()
-            codereturn.remove(course)
-            return codereturn
-    return False
-
-def text_from_courselist(prefix, courselist, suffix, errortext=''):
-    """Makes a string out of a list of courses.
-
-    :param prefix: Prefix string.
-    :param courselist: List with courses and lists of courses as elements.
-                       The sublists represent interchangeable courses.
-    :param suffix: Suffix string.
-    :param errortext: String to return if courselist is empty.
-                      Defaults to ''
-
-    :return: String that explains the rrelationship between the courses in the list.
-    """
-    text = ''
-    for level1 in courselist:
-        if type(level1) is list:
-            text += 'enten '
-            for level2 in level1:
-                text += level2 + ' eller '
-            text = text[:-7] + ' og '
-        else:
-            text += level1 + ' og '
-    text = text[:-4]
-    return prefix + text + suffix if text != '' else errortext
-
-def listify(element):
-    """Turns something into a list if it isn't already a list"""
-    return element if isinstance(element, list) else [element]
-
-def new_root(course, obligatory_list, recommended_list, checked_courses, course_df):
-    """
-
-    :param course: Course code, string.
-    :param obligatory_list:
-    :param recommended_list:
-    :param checked_courses:
-    
-    :return: 3-tuple with lists of obligatory, recommended and checked courses
-    """
-    print(type(obligatory_list))
-    new_obligatory, new_recommended, new_checked_courses = grow_roots(course, checked_courses, course_df)
-    obligatory_list.extend(listify(new_obligatory))
-    recommended_list.extend(listify(new_recommended))
-    checked_courses.extend(listify(new_checked_courses))
-
-    return obligatory_list, recommended_list, checked_courses
-
 def grow_roots(course, checked_courses, course_df):
     """Makes lists of courses that are obligatory and recommended precursors to a course.
 
@@ -116,59 +46,6 @@ def grow_roots(course, checked_courses, course_df):
 
         return obligatory_list, recommended_list, checked_courses
     return [], [], []
-
-def duplicate_or_super(this_list, other_list):
-    """Checks if two lists are identical or one is a superset of the other.
-
-    :param this_list: Nested list of courses.
-    :param other_list: Nested list of courses.
-
-    :return: Bool. True if duplicate or first is super, False if not.
-    """
-    this_copy = this_list.copy()
-    other_copy = other_list.copy()
-    for this_item in this_list:
-        for other_item in other_list:
-            if this_item == other_item:
-                this_copy.remove(this_item)
-                other_copy.remove(other_item)
-    if this_copy == []:
-        return True
-    else:
-        return False
-
-def untangle_roots(messy_roots):
-    """Cleans up list of courses, removing duplicate information.
-
-    Other than removing complete duplicates, it also removes redundant information.
-    Say that to take course A, you have to take course B and D. To take B, you must take C or D.
-    This function would return that you only have to take B and D to take course A.
-
-    :param messy_roots: Nested list of courses.
-
-    :return: Nested list of courses.
-    """
-    messy_roots_copy = list(messy_roots)[0].copy()
-    untangled_roots = []
-    if messy_roots_copy != []:
-        for messy_root in messy_roots[0]:
-            if isinstance(messy_root, str):
-                untangled_roots.append(messy_root)
-                messy_roots_copy.remove(messy_root)
-        
-        for this_messy_root in messy_roots_copy:
-            for this_messy_subroot in this_messy_root:
-                if this_messy_subroot in untangled_roots:
-                    messy_roots_copy.remove(this_messy_root)
-                    break
-
-        messy_roots_copy2 = messy_roots_copy.copy()
-        for i, j in itertools.combinations(range(len(messy_roots_copy)), 2):
-            if i < j and duplicate_or_super(messy_roots_copy2[i], messy_roots_copy2[j]):
-                messy_roots_copy.remove(messy_roots_copy2[j])
-    
-    untangled_roots.extend(messy_roots_copy)
-    return untangled_roots
 
 def search_single_course(course, course_df, flags):
     """Prints out text describing what courses must be taken before taking a given course.
@@ -213,7 +90,7 @@ def search_single_course(course, course_df, flags):
         print("Couldn't find a course with that course code, please try another.")
         return results
 
-    total_text = ['\nEmnet ', '']
+    total_text = ['', '']
 
     for index, other_course_row in course_df.iterrows():
 
@@ -239,7 +116,7 @@ def search_single_course(course, course_df, flags):
             not_done = obligatory_compound.requirements_not_implied_by(course_primitive)
             not_done.simplify()
 
-            total_text[0] += f"Leder til {other_course_row['coursecode']} - {other_course_row['coursename']}"\
+            total_text[0] += f"\nEmnet leder til {other_course_row['coursecode']} - {other_course_row['coursename']}"\
                            + f"{f', hvis du også tar {not_done}' if not_done else ''}."
             total_text[1] += f"{other_course_row['coursecode']} - {other_course_row['coursename']} (obligatorisk)"
 
@@ -252,7 +129,7 @@ def search_single_course(course, course_df, flags):
             recommended_compound = CompoundCourseList.from_nested_list(other_course_row['obligatory'])
             not_done = recommended_compound.requirements_not_implied_by(course_primitive)
             not_done.simplify()
-            total_text[0] += f"Er en anbefalt forkunnskap til {other_course_row['coursecode']} - {other_course_row['coursename']}"\
+            total_text[0] += f"Emnet er en anbefalt forkunnskap til {other_course_row['coursecode']} - {other_course_row['coursename']}"\
                            + f"{f', sammen med {not_done}' if not_done else ''}."
 
             total_text[1] += f"{other_course_row['coursecode']} - {other_course_row['coursename']} (anbefalt)"
@@ -264,9 +141,9 @@ def search_single_course(course, course_df, flags):
 
     chosen_text = total_text[text_index]
     
-    if total_text[0] != '\nEmnet ':
+    if total_text[0] != '':
         results = True
-        print(chosen_text, "\n\n---\n")
+        print(chosen_text, "---\n")
     else:
         print(f'Fant dessverre ingen emner {course} leder til.')
 
@@ -274,7 +151,6 @@ def search_single_course(course, course_df, flags):
 
 if __name__ == '__main__':
     course_df = pd.read_pickle('courses.pkl')
-    print(course_df)
     course_df.set_index('coursecode', drop=False, inplace=True)
 
     print('Skriv inn en emnekode du vil se hva slags muligheter gir senere. Skriv \"-help\" for å se kommandoer og få hjelp.')
